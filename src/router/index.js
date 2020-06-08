0a66eb5770a117af96060c106dcc05ffbc129d0f
jhh-5959 <13353375959@163.com>
2020-06-08 13:17:58 +0800

    构造项目,配置全局样式.路由.element-ui

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import login from "../views/Login";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect:"login"
+  },
+  {
+    path: "/login",
+    component:login
+  }
+];
+
+const router = new VueRouter({
+  routes,
+  mode:"hash"
+});
+
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="login">
+        <span>我是登录页面</span>
+        <el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+    }
+</script>
+
+<style scoped>
+    #login{
+        background-color: #344a5f;
+        height: 100vh;
+    }
+</style>
